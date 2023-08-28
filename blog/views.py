2fb69cd792e2869bdcc2e12cf304ead1936fb1dd
daniel <you@example.com>
2023-08-27 19:40:46 -0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+
+
+def render_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'posts.html',{'posts':posts})
+
+
+
+def post_detail(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post.detail.html', {"post":post})
+
+
+
+
+
+# Create your views here.
